2e13eaab96876c36729abc924ab2f7f3e240edfc
MyBazilio <myrellebazilio08@gmail.com>
2025-10-03 21:10:28 -0300

    Add files via upload

---
+++ Exercicio04_Repeticao_Decisao_Relacionais_Aritmeticos/senha.cs
@@ -0,0 +1,20 @@
+pusing System;
+ 
+class Programa {
+    static void Main() {
+        string senhaCorreta = "1234";
+        string senhaDigitada;
+ 
+        do {
+            Console.Write("Digite a senha: ");
+            senhaDigitada = Console.ReadLine();
+ 
+            if (senhaDigitada != senhaCorreta) {
+                Console.WriteLine("❌ Senha incorreta!");
+            }
+ 
+        } while (senhaDigitada != senhaCorreta);
+ 
+        Console.WriteLine("✅ Senha correta!");
+    }
+}
