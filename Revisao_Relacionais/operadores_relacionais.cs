a4adc4e3c9bcb88a557c33cc78cde7191be560d5
MyBazilio <myrellebazilio08@gmail.com>
2025-10-10 20:14:28 -0300

    Add files via upload

---
+++ Revisao_Relacionais/operadores_relacionais.cs
@@ -0,0 +1,12 @@
+using System;
+class Relational {
+    static void Main() {
+        int x = 10, y = 20;
+        Console.WriteLine($"x == y -> {x == y}");
+        Console.WriteLine($"x != y -> {x != y}");
+        Console.WriteLine($"x < y  -> {x < y}");
+        Console.WriteLine($"x <= y -> {x <= y}");
+        Console.WriteLine($"x > y  -> {x > y}");
+        Console.WriteLine($"x >= y -> {x >= y}");
+    }
+}
