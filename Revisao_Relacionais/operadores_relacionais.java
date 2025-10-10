808f1985d7599f54ed0eab944811943f5dce9840
MyBazilio <myrellebazilio08@gmail.com>
2025-10-10 20:17:18 -0300

    Add files via upload

---
+++ Revisao_Relacionais/operadores_relacionais.java
@@ -0,0 +1,12 @@
+
+public class operadores_relacionais {
+    public static void main(String[] args) {
+        int x = 10, y = 20;
+        System.out.println("x == y -> " + (x == y));
+        System.out.println("x != y -> " + (x != y));
+        System.out.println("x < y  -> " + (x < y));
+        System.out.println("x <= y -> " + (x <= y));
+        System.out.println("x > y  -> " + (x > y));
+        System.out.println("x >= y -> " + (x >= y));
+    }
+}
